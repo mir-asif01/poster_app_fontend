93e1bf95366ebe29f95c3f4e03a4c7b39b2366f9
mir-asif01 <asifbsb2002@gmail.com>
2024-07-04 21:19:10 +0600

    react-hook-form used to handle create post form data

--- src/components/main/Write.jsx
+++ src/components/main/Write.jsx
@@ -1,8 +1,23 @@
+import { useForm } from "react-hook-form";
+
 function Write() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    const post = new FormData();
+    post.append("title", data.title);
+    post.append("postImage", data.postImage[0]);
+    post.append("summary", data.summary);
+    post.append("content", data.content);
+
+    console.log(post[title]);
+  };
   return (
     <>
       <section className="px-10 py-32 flex justify-center items-center">
-        <form className="p-10 border border-slate-400 rounded-md w-2/4 ">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="p-10 border border-slate-400 rounded-md w-2/4 "
+        >
           <h1 className="text-4xl mb-7 text-center">Create Post</h1>
           <div className="mt-3">
             <label className="" htmlFor="password">
@@ -10,6 +25,7 @@ function Write() {
             </label>
             <br />
             <input
+              {...register("title")}
               className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
               type="text"
               name="title"
@@ -23,6 +39,7 @@ function Write() {
             </label>
             <br />
             <input
+              {...register("postImage")}
               className="outline-none border border-gray-300 rounded-md w-full file:bg-indigo-500 file:outline-none file:border-none file:px-2 file:py-2 file:text-white file:font-semibold"
               type="file"
               name="postImage"
@@ -36,6 +53,7 @@ function Write() {
             </label>
             <br />
             <input
+              {...register("summary")}
               className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
               type="text"
               name="summary"
@@ -49,6 +67,7 @@ function Write() {
             </label>
             <br />
             <textarea
+              {...register("content")}
               className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
               type=""
               name="content"
