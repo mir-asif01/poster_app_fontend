e9a5da6bab950f1c6215595b5b3d7b2d79559ca9
mir-asif01 <asifbsb2002@gmail.com>
2024-07-07 22:48:33 +0600

    responsive design added on about page

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -1,10 +1,16 @@
-function About(){
-    return <>
-        <section className="bg px-20 py-32">
-            <div className="flex justify-center items-center py-10">
-                <h1 className="text-6xl">Poster is a initiative to give programmers,software engineers and every other netizens the freedom of mind to write and earn acknowledgement in thier respective fields.</h1>
-            </div>
-        </section>
+function About() {
+  return (
+    <>
+      <section className="bg px-4 py-32 md:px-20 md:py-32">
+        <div className="flex justify-center items-center md:py-10">
+          <h1 className="text-3xl md:text-6xl">
+            Poster is a initiative to give programmers,software engineers and
+            every other netizens the freedom of mind to write and earn
+            acknowledgement in thier respective fields.
+          </h1>
+        </div>
+      </section>
     </>
+  );
 }
-export default About
+export default About;
