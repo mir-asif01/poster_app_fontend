fa5f068c8737fbb8c50ba82e44ef4ae176245bc1
mir-asif01 <asifbsb2002@gmail.com>
2024-08-20 20:12:09 +0600

    link to homepage added on the navbar logo

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,14 +1,14 @@
-import { createContext } from "react";
+import { createContext } from "react"
 
-export const Context = createContext();
+export const Context = createContext()
 
 export function ContextProvider({ children }) {
-  const user = { name: "asif" };
-  // const user = {};
-  const contextValue = { user };
-  return (
-    <>
-      <Context.Provider value={contextValue}>{children}</Context.Provider>
-    </>
-  );
+   const user = {}
+   // const user = {};
+   const contextValue = { user }
+   return (
+      <>
+         <Context.Provider value={contextValue}>{children}</Context.Provider>
+      </>
+   )
 }
