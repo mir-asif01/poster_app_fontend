8e13eea4a8f4105510514a42319498a2fbd299c9
mir-asif01 <asifbsb2002@gmail.com>
2024-08-20 20:07:04 +0600

    started working

--- src/components/main/Read.jsx
+++ src/components/main/Read.jsx
@@ -1,72 +1,72 @@
-import { NavLink } from "react-router-dom";
-import img from "../../assets/Login.png";
-import { FaRegBookmark, FaRegHeart } from "react-icons/fa";
-import PostCard from "../shared/PostCard";
-import { useState } from "react";
+import { NavLink } from "react-router-dom"
+import img from "../../assets/Login.png"
+import { FaRegBookmark, FaRegHeart } from "react-icons/fa"
+import PostCard from "../shared/PostCard"
+import { useState } from "react"
 
 function Read() {
-  const [activeTab, setActiveTab] = useState(null);
-  const handleTabClick = (e) => {
-    setActiveTab(e.target.name);
-  };
-  return (
-    <>
-      <section className="md:py-10 md:px-48">
-        <div className="border-b mb-10 border-slate-200 flex justify-between items-center">
-          <div className="flex justify-between items-center gap-10 px-2 py-4">
-            <div className="flex justify-center items-center gap-5">
-              <button
-                onClick={(e) => {
-                  handleTabClick(e);
-                }}
-                name="all"
-                className="md:mx-3 md:pb-3 md:px-5"
-              >
-                For You
-              </button>
-              <button
-                onClick={(e) => {
-                  handleTabClick(e);
-                }}
-                name="following"
-                className="md:mx-3 md:pb-3 md:px-5"
-              >
-                Following
-              </button>
+   const [activeTab, setActiveTab] = useState(null)
+   const handleTabClick = (e) => {
+      setActiveTab(e.target.name)
+   }
+   return (
+      <>
+         <section className="md:py-10 md:px-48">
+            <div className="border-b mb-10 border-slate-200 flex justify-between items-center">
+               <div className="flex justify-between items-center gap-10 px-2 py-4">
+                  <div className="flex justify-center items-center gap-5">
+                     <button
+                        onClick={(e) => {
+                           handleTabClick(e)
+                        }}
+                        name="all"
+                        className="md:mx-3 md:pb-3 md:px-5"
+                     >
+                        For You
+                     </button>
+                     <button
+                        onClick={(e) => {
+                           handleTabClick(e)
+                        }}
+                        name="following"
+                        className="md:mx-3 md:pb-3 md:px-5"
+                     >
+                        Following
+                     </button>
+                  </div>
+                  <div className="flex justify-between items-center">
+                     <div className="md:mr-5">Filter</div>
+                     <select
+                        name="Filter By Tags"
+                        className="px-10 py-1 outline-none border border-gray-300 rounded-md"
+                        id="Category"
+                     >
+                        <option value="one">one</option>
+                        <option value="one">one</option>
+                        <option value="one">one</option>
+                        <option value="one">one</option>
+                     </select>
+                  </div>
+               </div>
             </div>
-            <div className="flex justify-between items-center">
-              <div className="md:mr-5">Filter</div>
-              <select
-                name="Filter By Tags"
-                className="px-10 py-1 outline-none border border-gray-300 rounded-md"
-                id="Category"
-              >
-                <option value="one">one</option>
-                <option value="one">one</option>
-                <option value="one">one</option>
-                <option value="one">one</option>
-              </select>
+            <div className="col-span-7 p-5 grid grid-cols-1 md:grid-cols-2 gap-10 items-center justify-center">
+               {activeTab === "following" ? (
+                  <>
+                     <PostCard />
+                     <PostCard />
+                  </>
+               ) : (
+                  <>
+                     <PostCard />
+                     <PostCard />
+                     <PostCard />
+                     <PostCard />
+                  </>
+               )}
             </div>
-          </div>
-        </div>
-        <div className="col-span-7 p-5 grid grid-cols-1 md:grid-cols-2 gap-10 items-center justify-center">
-          {activeTab === "following" ? (
-            <>
-              <PostCard />
-              <PostCard />
-            </>
-          ) : (
-            <>
-              <PostCard />
-              <PostCard />
-              <PostCard />
-              <PostCard />
-            </>
-          )}
-        </div>
-      </section>
-    </>
-  );
+         </section>
+      </>
+   )
 }
 
-export default Read;
+export default Read
--- src/components/main/Write.jsx
+++ src/components/main/Write.jsx
@@ -1,110 +1,110 @@
-import { useForm } from "react-hook-form";
-import { Context } from "../../context/Context";
-import { useContext } from "react";
+import { useForm } from "react-hook-form"
+import { Context } from "../../context/Context"
+import { useContext } from "react"
 
 function Write() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = async (data) => {
-    console.log(data);
+   const { register, handleSubmit } = useForm()
+   const onSubmit = async (data) => {
+      console.log(data)
 
-    const post = new FormData();
-    post.append("title", data.title);
-    post.append("postImage", data.postImage[0]);
-    post.append("tags", data.tags);
-    post.append("summary", data.summary);
-    post.append("content", data.content);
+      const post = new FormData()
+      post.append("title", data.title)
+      post.append("postImage", data.postImage[0])
+      post.append("tags", data.tags)
+      post.append("summary", data.summary)
+      post.append("content", data.content)
 
-    console.log(post);
-  };
-  const { user } = useContext(Context);
-  return (
-    <>
-      <section className="p-2 md:px-10 py-3 md:py-32 md:flex justify-center items-center">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="p-2 md:p-10 border border-slate-400 rounded-md md:w-2/4 "
-        >
-          <h1 className="text-4xl mb-7 text-center">Create Post</h1>
-          <div className="mt-3">
-            <label className="" htmlFor="password">
-              Title
-            </label>
-            <br />
-            <input
-              {...register("title")}
-              className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
-              type="text"
-              name="title"
-              placeholder="Enter post title"
-              required
-            />
-          </div>
-          <div className="mt-3">
-            <label className="" htmlFor="password">
-              Post Image
-            </label>
-            <br />
-            <input
-              {...register("postImage")}
-              className="outline-none border border-gray-300 rounded-md w-full file:bg-indigo-500 file:outline-none file:border-none file:px-2 file:py-2 file:text-white file:font-semibold"
-              type="file"
-              name="postImage"
-              placeholder="Your post image"
-              required
-            />
-          </div>
-          <div className="mt-3">
-            <label className="" htmlFor="password">
-              Tags
-            </label>
-            <br />
-            <input
-              {...register("tags")}
-              className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
-              type="text"
-              name="tags"
-              placeholder="eg - javascript,reactjs..."
-              required
-            />
-          </div>
-          <div className="mt-3">
-            <label className="" htmlFor="password">
-              Summary
-            </label>
-            <br />
-            <input
-              {...register("summary")}
-              className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
-              type="text"
-              name="summary"
-              placeholder="Summary of your post"
-              required
-            />
-          </div>
-          <div className="mt-3">
-            <label className="" htmlFor="password">
-              Content
-            </label>
-            <br />
-            <textarea
-              {...register("content")}
-              className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
-              type=""
-              name="content"
-              placeholder="Content of your post"
-              required
-            />
-          </div>
-          <div className="flex justify-center items-center mt-5">
-            <input
-              className="bg-indigo-500 cursor-pointer font-semibold rounded-3xl text-white px-10 py-2"
-              type="submit"
-              value={"Publish"}
-            />
-          </div>
-        </form>
-      </section>
-    </>
-  );
+      console.log(post)
+   }
+   const { user } = useContext(Context)
+   return (
+      <>
+         <section className="p-2 md:px-10 py-3 md:py-32 md:flex justify-center items-center">
+            <form
+               onSubmit={handleSubmit(onSubmit)}
+               className="p-2 md:p-10 border border-slate-400 rounded-md md:w-2/4 "
+            >
+               <h1 className="text-4xl mb-7 text-center">Create Post</h1>
+               <div className="mt-3">
+                  <label className="" htmlFor="password">
+                     Title
+                  </label>
+                  <br />
+                  <input
+                     {...register("title")}
+                     className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
+                     type="text"
+                     name="title"
+                     placeholder="Enter post title"
+                     required
+                  />
+               </div>
+               <div className="mt-3">
+                  <label className="" htmlFor="password">
+                     Post Image
+                  </label>
+                  <br />
+                  <input
+                     {...register("postImage")}
+                     className="outline-none border border-gray-300 rounded-md w-full file:bg-indigo-500 file:outline-none file:border-none file:px-2 file:py-2 file:text-white file:font-semibold"
+                     type="file"
+                     name="postImage"
+                     placeholder="Your post image"
+                     required
+                  />
+               </div>
+               <div className="mt-3">
+                  <label className="" htmlFor="password">
+                     Tags
+                  </label>
+                  <br />
+                  <input
+                     {...register("tags")}
+                     className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
+                     type="text"
+                     name="tags"
+                     placeholder="eg - javascript,reactjs..."
+                     required
+                  />
+               </div>
+               <div className="mt-3">
+                  <label className="" htmlFor="password">
+                     Summary
+                  </label>
+                  <br />
+                  <input
+                     {...register("summary")}
+                     className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
+                     type="text"
+                     name="summary"
+                     placeholder="Summary of your post"
+                     required
+                  />
+               </div>
+               <div className="mt-3">
+                  <label className="" htmlFor="password">
+                     Content
+                  </label>
+                  <br />
+                  <textarea
+                     {...register("content")}
+                     className="outline-none border border-gray-300 rounded-md px-2 py-2 w-full"
+                     type=""
+                     name="content"
+                     placeholder="Content of your post"
+                     required
+                  />
+               </div>
+               <div className="flex justify-center items-center mt-5">
+                  <input
+                     className="bg-indigo-500 cursor-pointer font-semibold rounded-3xl text-white px-10 py-2"
+                     type="submit"
+                     value={"Publish"}
+                  />
+               </div>
+            </form>
+         </section>
+      </>
+   )
 }
-export default Write;
+export default Write
