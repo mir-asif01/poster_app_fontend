0679acc5e791ca1ce50e83bd3633ac2b4103a619
mir-asif01 <asifbsb2002@gmail.com>
2024-10-28 21:24:05 +0600

    comment drawer added and adding comment feature added

--- src/components/main/PostDetails.jsx
+++ src/components/main/PostDetails.jsx
@@ -10,12 +10,23 @@ import { useState } from "react"
 
 function PostDetails() {
   const [commentDrawerOpen, setCommentDrawerOpen] = useState(false)
+  const [commentArray, setCommentsArray] = useState([])
+  const [comment, setComment] = useState("")
   const userStr = localStorage.getItem("user")
   const loggedInUser = JSON.parse(userStr)
   const res = useLoaderData()
   const post = res.post
-  const { _id, creatorId, title, content, postImage, createdAt, likesCount } =
-    post
+  const {
+    _id,
+    creatorId,
+    creatorName,
+    creatorProfileImage,
+    title,
+    content,
+    postImage,
+    createdAt,
+    likesCount,
+  } = post
 
   const handleLikeButton = async () => {
     try {
@@ -39,15 +50,51 @@ function PostDetails() {
 
   const handleCommentsPanelOpen = async () => {
     setCommentDrawerOpen(true)
+  }
+
+  const fetchComments = async () => {
     try {
+      await axios
+        .get(`http://localhost:3000/comments-for-post?id=${_id}`)
+        .then((res) => {
+          if (res.data.success) {
+            setCommentsArray(res.data.comments)
+          }
+        })
     } catch (error) {
-      console.log(error)
+      console.log(error.message)
     }
   }
 
   const closeCommentDrawer = async () => {
     setCommentDrawerOpen(false)
   }
+
+  const handleAddComment = async () => {
+    try {
+      const newComment = {
+        comment: comment,
+        postId: _id,
+        commentorName: creatorName,
+        commentorProfileImage: creatorProfileImage,
+      }
+
+      await axios
+        .post("http://localhost:3000/add-comment", newComment)
+        .then((res) => {
+          if (res.data.success) {
+            toast.success(res.data.message)
+          }
+          if (!res.data.success) {
+            toast.error(res.data.message)
+          }
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  fetchComments()
 
   return (
     <section className="relative">
@@ -59,40 +106,40 @@ function PostDetails() {
           </div>
           <div className="px-4 py-3">
             {/* <div className="flex justify-between text-2xl items-center my-2">
-          <div className="flex gap-2 items-center">
-            <img src={img} className="w-5 h-5 rounded-full" alt="" />
-            <p className="font-semibold">Mir Kamrul Ahsan Asif</p>
-          </div>
-          <p className="font-semibold">Web Developer</p>
-        </div> */}
+              <div className="flex gap-2 items-center">
+                <img src={img} className="w-5 h-5 rounded-full" alt="" />
+                <p className="font-semibold">Mir Kamrul Ahsan Asif</p>
+              </div>
+              <p className="font-semibold">Web Developer</p>
+            </div> */}
             <div>
               <p className="text-4xl font-semibold">{title}</p>
             </div>
             <div className="flex justify-between items-center mt-3">
               <div className="flex justify-between gap-3 items-center mt-2 text-xl">
-                <p className="bg-slate-900 px-2 py-1 rounded-3xl text-white">
+                <p className="bg-slate-900 text-xs px-2 py-1 rounded-3xl text-white">
                   {new Date(createdAt).toLocaleDateString()}
                 </p>
-                <p className="bg-slate-900 px-2 py-1 rounded-3xl text-white">
+                <p className="bg-slate-900 text-xs px-2 py-1 rounded-3xl text-white">
                   {content.length > 450 ? Math.round(content.length / 450) : 1}{" "}
                   min read
                 </p>
-                <p className="bg-slate-900 px-2 py-1 rounded-3xl text-white">
+                <p className="bg-slate-900 text-xs px-2 py-1 rounded-3xl text-white">
                   {likesCount} {likesCount === 1 ? "Like" : "Likes"}
                 </p>
               </div>
               <div className="flex justify-between gap-6 items-center text-2xl">
                 <FaRegBookmark
-                  className="cursor-pointer"
+                  className="cursor-pointer text-2xl"
                   onClick={() => alert("Added to favorites")}
                 />{" "}
                 {/* add to bookmark button */}
                 <LuThumbsUp
-                  className="cursor-pointer text-3xl"
+                  className="cursor-pointer text-2xl"
                   onClick={handleLikeButton}
                 />
                 <FaComment
-                  className="cursor-pointer text-3xl"
+                  className="cursor-pointer text-2xl"
                   onClick={handleCommentsPanelOpen}
                 />
               </div>
@@ -111,13 +158,30 @@ function PostDetails() {
         } right-0 top-0 bg-slate-200 p-5 h-full overflow-x-hidden overflow-y-scroll`}
       >
         <div className="flex justify-between items-center mb-5">
-          <h1 className="text-2xl">Comments</h1>
+          <h1 className="text-2xl font-semibold">Comments</h1>
           <RxCross1
             className="text-2xl cursor-pointer"
             onClick={closeCommentDrawer}
           />
         </div>
-        <div className="p-5 bg-white rounded-lg my-10">
+        <div className="border p-5 rounded-lg bg-white">
+          <div>
+            <input
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              className="w-full rounded-lg border mb-2 outline-none p-2"
+              name="comment"
+              id=""
+            />
+          </div>
+          <button
+            onClick={handleAddComment}
+            className="bg-indigo-500 px-3 py-2 font-semibold text-white rounded-md"
+          >
+            Add Comment
+          </button>
+        </div>
+        <div className="p-5 bg-white rounded-lg my-5">
           <div className="flex justify-between items-center p-2 mb-2">
             <div>
               <img src={img} className="h-10 w-10" alt="" />
@@ -129,91 +193,10 @@ function PostDetails() {
           </div>
           <hr />
           <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
+            {commentArray ? commentArray.length : "no comment found"}
           </p>
         </div>
-        <div className="p-5 bg-white rounded-lg my-10">
-          <div className="flex justify-between items-center p-2 mb-2">
-            <div>
-              <img src={img} className="h-10 w-10" alt="" />
-            </div>
-            <div>
-              <h1 className="text-xl font-semibold">Commentor Name</h1>
-              <h1 className="">10/10/2024</h1>
-            </div>
-          </div>
-          <hr />
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
-          </p>
-        </div>
-        <div className="p-5 bg-white rounded-lg my-10">
-          <div className="flex justify-between items-center p-2 mb-2">
-            <div>
-              <img src={img} className="h-10 w-10" alt="" />
-            </div>
-            <div>
-              <h1 className="text-xl font-semibold">Commentor Name</h1>
-              <h1 className="">10/10/2024</h1>
-            </div>
-          </div>
-          <hr />
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
-          </p>
-        </div>
-        <div className="p-5 bg-white rounded-lg my-10">
-          <div className="flex justify-between items-center p-2 mb-2">
-            <div>
-              <img src={img} className="h-10 w-10" alt="" />
-            </div>
-            <div>
-              <h1 className="text-xl font-semibold">Commentor Name</h1>
-              <h1 className="">10/10/2024</h1>
-            </div>
-          </div>
-          <hr />
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
-          </p>
-        </div>
-        <div className="p-5 bg-white rounded-lg my-10">
-          <div className="flex justify-between items-center p-2 mb-2">
-            <div>
-              <img src={img} className="h-10 w-10" alt="" />
-            </div>
-            <div>
-              <h1 className="text-xl font-semibold">Commentor Name</h1>
-              <h1 className="">10/10/2024</h1>
-            </div>
-          </div>
-          <hr />
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
-          </p>
-        </div>
-        <div className="p-5 bg-white rounded-lg my-10">
-          <div className="flex justify-between items-center p-2 mb-2">
-            <div>
-              <img src={img} className="h-10 w-10" alt="" />
-            </div>
-            <div>
-              <h1 className="text-xl font-semibold">Commentor Name</h1>
-              <h1 className="">10/10/2024</h1>
-            </div>
-          </div>
-          <hr />
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            laudantium.
-          </p>
-        </div>
-        <div className="p-5 bg-white rounded-lg my-10">
+        <div className="p-5 bg-white rounded-lg my-5">
           <div className="flex justify-between items-center p-2 mb-2">
             <div>
               <img src={img} className="h-10 w-10" alt="" />
